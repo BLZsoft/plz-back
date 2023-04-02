24d45a20530504aefbeda3ea9236074176b008f1
Danil Tankov <danil.tankoff@yandex.ru>
2023-04-02 20:53:16 +0700

    fix(swagger): /api to /api/ redirect

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,6 +1,7 @@
 import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import type { NextFunction, Request, Response } from 'express';
 
 import { Config } from 'common/config';
 
@@ -11,8 +12,7 @@ export const initializeSwagger = (app: NestExpressApplication): void => {
   const swaggerDocumentBuilder = new DocumentBuilder()
     .setTitle('пожликбез.рф API')
     .setDescription('API спецификация пожликбез.рф')
-    .setVersion('1.0')
-    .setContact('evist0', '', 'danil.tankoff@yandex.ru');
+    .setVersion('1.0');
 
   if (!commonConfig.isTest) {
     swaggerDocumentBuilder.addBearerAuth();
@@ -22,6 +22,15 @@ export const initializeSwagger = (app: NestExpressApplication): void => {
     app,
     swaggerDocumentBuilder.build()
   );
+
+  const expressApp = app.getHttpAdapter();
+  expressApp.use((req: Request, res: Response, next: NextFunction) => {
+    if (req.url === '/api') {
+      return res.redirect('/api/');
+    }
+
+    return next();
+  });
 
   SwaggerModule.setup('', app, swaggerDocument, {
     useGlobalPrefix: true,
